d1b4991899f3dbac8456e473d790a711ed9184e1
Liang <william810212@gmail.com>
2021-03-03 17:49:11 +0800

    add vue-lazyload

--- nuxt.config.js
+++ nuxt.config.js
@@ -121,6 +121,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    // 安裝的套件
+    { src: '~/plugins/vue-lazyload', mode: 'client' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
