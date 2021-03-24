5495c1cd74e62bff000156870c3e358b5b7097b1
Liang <william810212@gmail.com>
2021-03-24 11:08:57 +0800

    add main swiper

---
+++ components/Swiper/Main/AudiobookSlide.vue
@@ -0,0 +1,222 @@
+<template>
+  <div class="swiper-slide">
+    <div class="relative flex justify-center lg:p-12 lg:items-center lg:h-full">
+      <div
+        class="container flex flex-wrap items-center justify-center h-full lg:justify-between lg:flex-nowrap"
+      >
+        <div class="cover">
+          <img
+            v-lazy=""
+            class="object-cover w-full h-full"
+          >
+        </div>
+        <div class="info">
+          <div
+            class="relative h-full px-6 py-4 ml-8 bg-white lg:px-10 lg:py-8 xl:ml-16 lg:ml-12"
+          >
+            <div class="flex items-center mb-2">
+              <NuxtLink
+                :to="{
+                  name: 'audiobooks',
+                }"
+                class="inline-block px-2 mr-2 text-sm tracking-wide text-white lg:text-lg bg-blue-light"
+              >
+                有聲書
+              </NuxtLink>
+              <template v-for="(category, index) in 5">
+                <NuxtLink
+                  v-if="index === 0"
+                  :key="`audiobook_category_${category}`"
+                  :to="{
+                    name: 'audiobooks',
+                    query: {
+                      category: category,
+                    },
+                  }"
+                  class="text-sm lg:text-lg text-blue-light"
+                >
+                  有聲書分類
+                </NuxtLink>
+              </template>
+            </div>
+            <NuxtLink
+              :to="{
+                name: 'audiobooks-audiobook',
+                params: { audiobook: 1 },
+              }"
+              class="block mb-3 text-base tracking-wider text-black truncate lg:text-4xl"
+            >
+              有聲書名稱
+            </NuxtLink>
+            <p
+              class="mb-4 text-sm text-gray-700 lg:text-xl line-clamp line-clamp-2"
+            >
+              介紹介紹介紹介紹介紹
+            </p>
+            <div class="truncate">
+              <span
+                class="pr-2 mr-2 text-sm text-gray-700 border-r border-gray-700 lg:text-base"
+              >主播</span>
+              <template v-for="anchor in 1">
+                <NuxtLink
+                  :key="`anchor_${anchor}`"
+                  :to="{
+                    name: 'voices-anchor-id',
+                    params: { id: anchor },
+                  }"
+                  class="text-sm text-gray-700 lg:text-base symbol"
+                >
+                  主播名稱
+                </NuxtLink>
+              </template>
+            </div>
+            <footer
+              class="absolute bottom-0 -mb-4 transform -translate-x-1/2 lg:-mb-8 left-50"
+            >
+              <NuxtLink
+                class="flex items-center justify-center w-32 py-2 mx-auto text-sm tracking-widest lg:text-lg lg:w-48 lg:py-4 btn btn-secondary"
+                :to="{
+                  name: 'audiobooks-audiobook',
+                  params: { audiobook: 1 },
+                }"
+              >
+                前往收聽
+                <svg
+                  class="w-4 h-4 ml-1"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="chevron-right"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"
+                  />
+                </svg>
+              </NuxtLink>
+            </footer>
+          </div>
+        </div>
+      </div>
+      <!-- 背景圖 -->
+      <div class="bg-mask">
+        <img
+          v-lazy=""
+          class="bg-mask__cover"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainAudiobookSlide'
+}
+</script>
+
+<style lang="postcss" scoped>
+.bg-mask {
+  @apply absolute top-0 left-0 w-full bg-black overflow-hidden;
+
+  height: 250px;
+
+  &__cover {
+    @apply absolute w-full left-50 top-50 opacity-50 transform -translate-x-1/2 -translate-y-1/2;
+
+    filter: blur(20px);
+    -webkit-filter: blur(20px);
+    -moz-filter: blur(20px);
+    -ms-filter: blur(20px);
+  }
+}
+
+.cover {
+  @apply mt-12 shadow-lg z-10;
+
+  width: 200px;
+  height: 200px;
+}
+
+.info {
+  @apply bg-secondary bg-opacity-50 w-5/6 z-10 shadow-xl transform ml-0;
+
+  height: 200px;
+  transform: translateY(-50px);
+
+  &::after {
+    @apply absolute bg-opacity-50 w-5/6 h-full -z-10;
+
+    content: '';
+    top: -15px;
+    right: -15px;
+    background: repeating-linear-gradient(
+      -45deg,
+      transparent,
+      transparent 3px,
+      rgba(255, 109, 45, 0.8) 0,
+      rgba(255, 109, 45, 0.8) 6px
+    );
+  }
+}
+
+.swiper-slide .info {
+  @apply transition duration-150 ease-linear delay-200 opacity-0;
+}
+
+.swiper-slide-active .info {
+  @apply opacity-100;
+
+  transition-duration: 0;
+  transition-delay: 0;
+}
+
+.symbol:not(:last-child)::after {
+  content: '、';
+}
+
+@screen lg {
+  .bg-mask {
+    @apply h-full;
+  }
+
+  .cover {
+    @apply mt-0;
+
+    width: 380px;
+    height: 380px;
+  }
+
+  .info {
+    @apply w-1/2;
+
+    height: 340px;
+    transform: translateY(0);
+
+    &::after {
+      @apply w-4/5;
+
+      top: -25px;
+      right: -25px;
+    }
+  }
+}
+
+@screen xl {
+  .cover {
+    width: 450px;
+    height: 450px;
+  }
+
+  .info {
+    width: 58%;
+
+    &::after {
+      @apply w-5/6;
+    }
+  }
+}
+</style>
---
+++ components/Swiper/Main/Main.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="relative">
+    <div v-swiper:mainSwiper="swiperOption">
+      <div class="swiper-wrapper">
+        <template v-for="item in 1">
+          <SwiperMainPodcastSlide
+            :key="`main_swiper_podcast_${item}`"
+          />
+          <SwiperMainAudiobookSlide
+            :key="`main_swiper_audiobook_${item}`"
+          />
+          <SwiperMainCourseSlide
+            :key="`main_swiper_course_${item}`"
+          />
+          <SwiperMainDiscoverSlide
+            :key="`main_swiper_discover_${item}`"
+          />
+        </template>
+      </div>
+    </div>
+
+    <div class="main-swiper-pagination swiper-pagination" />
+    <div class="main-swiper-prev swiper-button-prev">
+      <svg
+        class="w-4 h-4 lg:w-5 lg:h-5 text-secondary"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="arrow-left"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path
+          fill="currentColor"
+          d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"
+        />
+      </svg>
+    </div>
+    <div class="main-swiper-next swiper-button-next">
+      <svg
+        class="w-4 h-4 lg:w-5 lg:h-5 text-secondary"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fas"
+        data-icon="arrow-right"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <path
+          fill="currentColor"
+          d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SwiperMain',
+  data () {
+    return {
+      swiperOption: {
+        // loop: true,
+        touchRatio: 1,
+        slidesPerView: 1,
+        spaceBetween: 0,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false
+        },
+        navigation: {
+          nextEl: '.main-swiper-next',
+          prevEl: '.main-swiper-prev'
+        },
+        pagination: {
+          el: '.main-swiper-pagination',
+          clickable: true
+        },
+        breakpoints: {
+          // when window width is >= 1024px
+          1024: {
+            watchSlidesVisibility: true,
+            // loopedSlides: 0,
+            touchRatio: 0,
+            slidesPerView: 'auto',
+            spaceBetween: 30
+          }
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style lang="postcss" scoped>
+>>> .swiper-wrapper {
+  height: 540px;
+}
+
+>>> .swiper-slide {
+  @apply opacity-75 z-0;
+}
+
+>>> .swiper-slide-active {
+  @apply opacity-100 z-10;
+}
+
+>>> .swiper-pagination {
+  @apply flex items-center h-8 bottom-0 ml-8 mb-16;
+
+  .swiper-pagination-bullet {
+    @apply bg-white w-3 h-3 mr-2 rounded-full opacity-50 cursor-pointer;
+  }
+
+  .swiper-pagination-bullet-active {
+    @apply opacity-100;
+  }
+}
+
+>>> .swiper-button-prev,
+>>> .swiper-button-next {
+  @apply top-auto right-0 bottom-0 left-auto bg-white w-8 h-8 mb-16 rounded-full;
+
+  &::after {
+    content: none;
+  }
+}
+
+>>> .swiper-button-prev {
+  @apply mr-20;
+}
+
+>>> .swiper-button-next {
+  @apply mr-8;
+}
+
+@screen lg {
+  >>> .swiper-wrapper {
+    height: 580px;
+  }
+
+  >>> .swiper-slide {
+    width: 100% !important;
+  }
+
+  >>> .swiper-pagination {
+    @apply right-0 mr-8 mb-8;
+  }
+
+  >>> .swiper-button-prev,
+  >>> .swiper-button-next {
+    @apply w-12 h-12;
+  }
+
+  >>> .swiper-button-prev {
+    @apply mr-24;
+  }
+}
+</style>
